490b683119b8abac3060d4d878042a2da6edacb3
Troy Mathis <troy.mathis00@gmail.com>
2023-02-07 19:47:48 -0600

    part1

--- 2022/day1/most_cals.py
+++ 2022/day1/most_cals.py
@@ -1,7 +1,22 @@
-elves1 = "1000\n5000"
-print(elves1)
+elves1 = ""
+with open('input.txt', 'r') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        elves1 += line
+
 def mostCals(elves):
-    elves = elves.split("\n")
-    return elves
+    elves = elves.split("\n\n")
+    Sum = 0
+    for i in range(len(elves)):
+        elves[i] = elves[i].split("\n")
+        for j in range(len(elves[i])):
+            elves[i][j] = int(elves[i][j])
+        if sum(elves[i]) > Sum:
+            Sum = sum(elves[i])
+    
+
+    return Sum
 
 print(mostCals(elves1))
