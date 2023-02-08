80513a244dad48f4fdcb12107da7ca828608b376
Troy Mathis <troy.mathis00@gmail.com>
2023-02-07 22:38:41 -0600

    part2 solution

--- 2022/day1/most_cals.py
+++ 2022/day1/most_cals.py
@@ -8,15 +8,13 @@ with open('input.txt', 'r') as f:
 
 def mostCals(elves):
     elves = elves.split("\n\n")
-    Sum = 0
+    Sums = []
     for i in range(len(elves)):
         elves[i] = elves[i].split("\n")
         for j in range(len(elves[i])):
             elves[i][j] = int(elves[i][j])
-        if sum(elves[i]) > Sum:
-            Sum = sum(elves[i])
-    
-
-    return Sum
+        Sums.append(sum(elves[i]))
+    Sums.sort()
+    return Sums[-1]+Sums[-2]+Sums[-3]
 
 print(mostCals(elves1))
