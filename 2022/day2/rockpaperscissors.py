70b6b05548252ea874c1fbead967cc78ce667e3b
Troy Mathis <troy.mathis00@gmail.com>
2023-02-13 19:20:20 -0600

    part2 solved

--- 2022/day2/rockpaperscissors.py
+++ 2022/day2/rockpaperscissors.py
@@ -32,15 +32,43 @@ def pickScore(appendedList):
 
 def compare(appendedList):
     for x in range(len(appendedList)):
+            # draw condition
         if appendedList[x][0] == appendedList[x][1]:
             appendedList[x][1] += 3
+            # lose conditions
         elif (appendedList[x][0] == 2 and appendedList[x][1] == 1) or (appendedList[x][0] == 1 and appendedList[x][1] == 3) or (appendedList[x][0] == 3 and appendedList[x][1] == 2):
             appendedList[x][1] += 0
+            # win conditions
         elif (appendedList[x][0] == 1 and appendedList[x][1] == 2) or (appendedList[x][0] == 3 and appendedList[x][1] == 1) or (appendedList[x][0] == 2 and appendedList[x][1] == 3):
             appendedList[x][1] += 6
     return appendedList
 
-scores = compare(pickScore(splitInputs(games)))
+def partTwo(appendedList):
+    for x in range(len(appendedList)):
+        if appendedList[x][1] == 1:
+            if appendedList[x][0] == 1:
+                appendedList[x][1] = 3
+            if appendedList[x][0] == 2:
+                appendedList[x][1] = 1
+            if appendedList[x][0] == 3:
+                appendedList[x][1] = 2
+        elif appendedList[x][1] == 2:
+            if appendedList[x][0] == 1:
+                appendedList[x][1] = 4
+            if appendedList[x][0] == 2:
+                appendedList[x][1] = 5
+            if appendedList[x][0] == 3:
+                appendedList[x][1] = 6
+        elif appendedList[x][1] == 3:
+            if appendedList[x][0] == 1:
+                appendedList[x][1] = 8
+            if appendedList[x][0] == 2:
+                appendedList[x][1] = 9
+            if appendedList[x][0] == 3:
+                appendedList[x][1] = 7
+    return appendedList
+
+scores = partTwo(pickScore(splitInputs(games)))
 
 total = 0
 for x in range(len(scores)):
