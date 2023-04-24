b86286b0a87b7aedc441e503c73ab923bb651dfb
Troy Mathis <troy.mathis00@gmail.com>
2023-04-24 17:09:33 -0500

    ranges printed

--- 2022/day4/camp_cleanup.py
+++ 2022/day4/camp_cleanup.py
@@ -13,4 +13,24 @@ def splitPairs(pairs):
         draft_pairs.append(newPairs[i].split(","))
     return draft_pairs
 
-print(splitPairs(pairs))
+ranges_final = []
+def findContainers(splitPairs):
+    for i in range(len(splitPairs)):
+        ranges = []
+        ranges2 = []
+        boundary1_2 = int(splitPairs[i][1][0])
+        boundary2_2 = int(splitPairs[i][1][2])
+        boundary2 = int(splitPairs[i][0][2])
+        boundary1 = int(splitPairs[i][0][0])
+        integer = 0
+        integer2 = 0
+        while integer < (boundary2-boundary1+1):
+            ranges.append(boundary1+integer)
+            integer += 1
+        while integer2 < (boundary2_2 - boundary1_2+1):
+            ranges2.append(boundary1_2+integer2)
+            integer2 +=1
+        ranges_final.append([ranges, ranges2])
+    return ranges_final
+
+print(findContainers(splitPairs(pairs)))
