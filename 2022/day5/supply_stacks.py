dfbbb1c9d45c1d3358dbdd21e4274c46edd1a13c
Troy Mathis <troy.mathis00@gmail.com>
2023-05-05 14:28:13 -0500

    stack formula

--- 2022/day5/supply_stacks.py
+++ 2022/day5/supply_stacks.py
@@ -5,12 +5,13 @@ with open('demo_input.txt', 'r') as f:
 number_of_crates = len(lines[0])//4
 crate_lines = lines[:lines.index('\n')-1]
 moving_lines = lines[lines.index('\n')+1:]
+
 def makeStacks(stacks):
     obj = [[] for i in range(number_of_crates)]
     for cratelines in crate_lines:
         print(cratelines)
         for i in range(1, len(cratelines), 4):
-            print(i)
+            obj[i//4].append(cratelines[i])
     return obj
 
 print(makeStacks(crate_lines))
